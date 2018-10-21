976630c57353f43c2f5125edfdc341b9298c903c
Rajdeep Das <irajdeep94@gmail.com>
2018-10-21 18:46:21 +0200

    Changes to dep

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
-package main
-
-import (
-)
-
-func main() {
-	
-}
+//package main
+//
+//import (
+//)
+//
+//func main() {
+//
+//}
--- cmd/monitor.go
+++ cmd/monitor.go
@@ -2,5 +2,18 @@ package main
 
 // Receiver channel from parser
 func processMonitorData(statsData <-chan AggregatedStats) {
+	// get the desired data from the channel
+	endPointStat, requestStatusStat := <-statsData
+
+	tatalHits := 0
+	maxHHitEndpoint := ""
+
+	for k, v := range endPointStat.EndPointStats {
+
+	}
+
+	for k, v := range endPointStat.RequestStatusStats {
+
+	}
 
 }
