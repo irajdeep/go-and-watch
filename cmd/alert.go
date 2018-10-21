5ce208054e7f669541d0896181f34c6dc6e85e61
Rajdeep Das <irajdeep94@gmail.com>
2018-10-21 19:56:49 +0200

    First draft implementation of alert module

--- cmd/alert.go
+++ cmd/alert.go
@@ -1,5 +1,20 @@
 package main
 
-func assertAndDisplay() {
+import "log"
 
+// Receives channel from main.go
+// channel should have data for alert interval , i.e 1 min
+func validateAndDisplayAlert(statsData <-chan AggregatedStats) {
+	// Should be taken from command line parameter in main.go
+	alertThreshold := int64(20)
+	alertCount := 0
+
+	monitorStat, _ := <-statsData
+
+	for _, element := range monitorStat.EndPointStats {
+		if element.hits > alertThreshold {
+			alertCount += 1
+		}
+	}
+	log.Printf("Number of endpoints with unsual high traffic %d", alertCount)
 }
