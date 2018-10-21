7c1632f7ced8761f859e6fba2a5e41d973d85f74
Rajdeep Das <irajdeep94@gmail.com>
2018-10-21 19:26:19 +0200

    Made monitor modular

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -6,15 +6,21 @@ import (
 
 // Receiver channel from parser
 
-func processAndDisplayMonitorData(statsData <-chan AggregatedStats) {
+func processAndMonitor(statsData <-chan AggregatedStats) {
 	// get the desired data from the channel
 	endPointStat, requestStatusStat := <-statsData
+	monitorEndpoint(endPointStat.EndPointStats)
+	monitorStatusCode(endPointStat.RequestStatusStats)
+
+}
+
+func monitorEndpoint(endPointStat []EndPointStat) {
 
 	maxHits := int64(0)
 	totalHits := int64(0)
 	maxHitEndpoint := ""
 
-	for _, element := range endPointStat.EndPointStats {
+	for _, element := range endPointStat {
 		// index is the index where we are
 		// element is the element from someSlice for where we are
 		if element.hits > maxHits {
@@ -24,5 +30,9 @@ func processAndDisplayMonitorData(statsData <-chan AggregatedStats) {
 		totalHits += element.hits
 	}
 	log.Printf("Maximum hit endpoint %s", maxHitEndpoint)
+
+}
+
+func monitorStatusCode(requestStatusStats []RequestStatusStat) {
 
 }
