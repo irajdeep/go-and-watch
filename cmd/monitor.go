1ba40069f2b6b4461201973e4d2077ec57e3401b
Rajdeep Das <irajdeep94@gmail.com>
2018-10-21 19:16:30 +0200

    Added logic for monitor function for endpoint

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -1,19 +1,28 @@
 package main
 
+import (
+	"log"
+)
+
 // Receiver channel from parser
-func processMonitorData(statsData <-chan AggregatedStats) {
+
+func processAndDisplayMonitorData(statsData <-chan AggregatedStats) {
 	// get the desired data from the channel
 	endPointStat, requestStatusStat := <-statsData
 
-	tatalHits := 0
-	maxHHitEndpoint := ""
+	maxHits := int64(0)
+	totalHits := int64(0)
+	maxHitEndpoint := ""
 
-	for k, v := range endPointStat.EndPointStats {
-
+	for _, element := range endPointStat.EndPointStats {
+		// index is the index where we are
+		// element is the element from someSlice for where we are
+		if element.hits > maxHits {
+			maxHits = element.hits
+			maxHitEndpoint = element.EndPoint
+		}
+		totalHits += element.hits
 	}
-
-	for k, v := range endPointStat.RequestStatusStats {
-
-	}
+	log.Printf("Maximum hit endpoint %s", maxHitEndpoint)
 
 }
